79ba5512ee07ce5b53b2ec91e9813185025b35c5
Dario Jularic <dariojularic0@gmail.com>
2024-05-27 22:52:22 +0200

    Create render function

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 import './style.css'
 
 const grid = document.querySelector(".grid");
+const start = document.querySelector(".start");
+
 
 class Square{
   constructor(color) {
     this.color = color;
+    this.clicked = false;
+  }
+
+  isClicked() {
+    this.clicked = true;
   }
 }
 
@@ -12,34 +19,49 @@ class SquareManager{
   sqareArray;
   constructor() {
     this.sqareArray = [];
-    this.numOfColumns = 4;
-    this.numOfRows = 3;
   }
 
   addSqare(square) {
     this.sqareArray.push(square);
   }
 
-  mixSquares() {
-    this.sqareArray.forEach(cell => {
-      cell.style.setProperty("grid-column", `1 / span 1`)
-      cell.style.setProperty("grid-row", `1 / span 1`)
+  renderSqares() {
+    this.sqareArray.forEach(sqare => {
+      const cell = document.createElement("div")
+      cell.style.backgroundColor = sqare.color;
+      grid.appendChild(cell);
     })
   }
 
+  shuffleSquares() {
+    let currentIndex = this.sqareArray.length;
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [this.sqareArray[currentIndex], this.sqareArray[randomIndex]] = [this.sqareArray[randomIndex], this.sqareArray[currentIndex]];
+    }
+  }
 
-}
-
-
-
-function makeGrid() {
-  grid.style.setProperty("grid-template-columns", "1fr 1fr 1fr 1fr");
-  grid.style.setProperty("grid-template-rows", "1fr 1fr 1fr");
-  for (let c = 0; c < 12; c++) {
-    const cell = document.createElement("div");
-    cell.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    grid.appendChild(cell).className = "grid-cell";
+  makeGrid() {
+    grid.style.setProperty("grid-template-columns", "1fr 1fr 1fr 1fr");
+    grid.style.setProperty("grid-template-rows", "1fr 1fr 1fr");
+    for (let c = 0; c < 12; c++) {
+      const cell = document.createElement("div");
+      cell.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+      grid.appendChild(cell).className = "grid-cell";
+    }
   }
 }
 
-makeGrid()
+const sqareManager = new SquareManager();
+sqareManager.makeGrid() 
+
+start.addEventListener("click", ()  => {
+
+})
+
+grid.addEventListener("click", (event) => {
+  if (event.target.className === "grid-cell") {
+
+  }
+})
