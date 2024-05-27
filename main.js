93e29d5ffcc1d5635ba6ee1cb624630edbf43671
Dario Jularic <dariojularic0@gmail.com>
2024-05-27 21:23:25 +0200

    Create classes

--- main.js
+++ main.js
@@ -2,6 +2,36 @@ import './style.css'
 
 const grid = document.querySelector(".grid");
 
+class Square{
+  constructor(color) {
+    this.color = color;
+  }
+}
+
+class SquareManager{
+  sqareArray;
+  constructor() {
+    this.sqareArray = [];
+    this.numOfColumns = 4;
+    this.numOfRows = 3;
+  }
+
+  addSqare(square) {
+    this.sqareArray.push(square);
+  }
+
+  mixSquares() {
+    this.sqareArray.forEach(cell => {
+      cell.style.setProperty("grid-column", `1 / span 1`)
+      cell.style.setProperty("grid-row", `1 / span 1`)
+    })
+  }
+
+
+}
+
+
+
 function makeGrid() {
   grid.style.setProperty("grid-template-columns", "1fr 1fr 1fr 1fr");
   grid.style.setProperty("grid-template-rows", "1fr 1fr 1fr");
